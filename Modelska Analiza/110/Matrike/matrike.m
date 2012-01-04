4fef15c806d7aa2e35bcf1da1737ad621c2fd3a5
Miha Čančula <miha@noughmad.eu>
2012-01-04 21:42:07 +0100

    Modelska, never ending story

--- Modelska Analiza/110/Matrike/matrike.m
+++ Modelska Analiza/110/Matrike/matrike.m
@@ -1,8 +1,11 @@
-N = 5;
+global M
+global N
+N = 250;
 M = N+1;
 
 smrtnost = 1.0;
-dt = 0.01;
+dt = 0.001;
+K = 150;
 
 function P = verjetnost(sprememba, povprecje)
   if povprecje == 0
@@ -12,14 +15,23 @@ function P = verjetnost(sprememba, povprecje)
   endif
 endfunction
 
+function A = povprecje(S)
+  global N
+  A = sum((N:-1:0)' .* S);
+endfunction
+
+function D = odmik(S, A)
+  global N
+  MS = sum((N:-1:0)' .* (N:-1:0)' .* S);
+  D = sqrt(MS - A^2);
+endfunction
+
 W = zeros(M);
 for j=1:M
-  s = dt * smrtnost * j;
+  s = dt * smrtnost * (M-j);
   for i=1:M
     if i > j
       W(i,j) = verjetnost(i-j, s);
-    elseif i < j
-      W(i,j) = 0;
     else
       W(i,j) = 0;
     endif
@@ -27,12 +39,22 @@ for j=1:M
   W(j,j) = 1 - sum(W(1:M,j));
 endfor
 
-W = W^100
+W = W^K;
 
 S = [1; zeros(N,1)];
-for i=1:10
+R = [0, 0, 1, 0];
+v = 0;
+T = dt * K;
+for i=1:100
   S = W * S;
-  S'
+  A = povprecje(S);
+  R = [R; i * T, S(M)-v, A/N, odmik(S,A)/sqrt(N)];
+  v = S(M);
 endfor
+
+Ostanek = 1-v
+Normalizacija = sum(S)
+
+save "g_matrike_exp_250.dat" R
 
 
