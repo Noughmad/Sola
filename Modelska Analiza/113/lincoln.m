8d7cb3eb372851f48737445294173695fe74be38
Miha Čančula <miha@noughmad.eu>
2012-02-14 12:36:35 +0100

    Vaja 113

--- Modelska Analiza/113/lincoln.m
+++ Modelska Analiza/113/lincoln.m
@@ -1,22 +1,51 @@
+global TaPrav
+
 function [X, Y, V] = pgmread(s)
-	V = dlmread(s)';
+	fid = fopen(s, "r");
+	fscanf(fid, "%c", 3);
+	[V, count] = fscanf(fid, "%d");
 	L = max(size(V));
-	X = V(2);
-	Y = V(3);
-	V = V(5:L) ./ V(4);
-	V = reshape(reshape(V, X, Y)', X*Y, 1);
+	X = V(1);
+	Y = V(2);
+	X, Y, X*Y, count
+	assert(X*Y == count-3)
+	Max = V(3);
+	V = V(4:L);
+	V = V ./ Max;
+
+	# Branje datoteke gre po vrsticah, zato jo najprej zlozimo v matriko enake dimenzije kot slika
+	V = reshape(V, X, Y);
+
+	# Sama slika pa je odbrana po stolpcih, zato jo transponiramo in zlozimo v en velik stolpec
+	V = reshape(V', X*Y, 1);
+	fclose(fid)
 endfunction
 
 function pgmwrite(s, V, X, Y)
 	# TODO
+	fid = fopen(s, "w");
+	fprintf(fid, "P2\n%d %d\n%d\n", X, Y, 255);
+	V = 255 .* V;
+	V = reshape(V, Y, X);
+	V = reshape(V', X*Y, 1);
+	X, Y
+	for i = 1:X*Y
+		V(i) = max(min(V(i), 255), 0);
+		if (mod(i, 19) != 0)
+			fprintf(fid, "%d ", V(i));
+		else
+			fprintf(fid, "%d\n", V(i));
+		endif
+	endfor
+	fclose(fid)
 endfunction
 
 function [X, Y, V] = linread(i)
-	[X, Y, V] = pgmread(["Lin" int2str(i) ".dat"]);
+	[X, Y, V] = pgmread(["lincoln_L30_N" int2str(i) "0.pgm"]);
 endfunction
 
-function lincoln(i)
-
+function Iz = lincoln(i, popravi)
+	global TaPrav
 	[X, Y, L] = linread(i);
 	SizeOfL = size(L)
 
@@ -25,17 +54,24 @@ function lincoln(i)
 	SL = reshape(L, N, 1);
 	FL = fft(SL);
 
-	semilogy(linspace(0,1,N), abs(FL).^2);
-	xlabel ("Frekvenca $f$")
-	ylabel ("$|C(f)|^2$")
-	print -depslatex g_fft_lincoln "-S640,480" 
+	tau = 30;
 
-	R = fft(exp(-linspace(0, N, N)/30))';
-	R(R < eps) = 1;
-
-	Iz = abs(ifft(FL ./ R));
-
-	pgmwrite(["g_lin_" int2str(i) ".dat"], Iz, X, Y);
+	R = fft(exp(-linspace(N, 0, N)/tau)/tau)';
+	# R(R < eps) = 1;
+	
+	if (popravi)
+		Iz = FL ./ R .* abs(TaPrav./FL).^2;
+		pgmwrite(["g_lincoln_popravljen_" int2str(i) ".pgm"], abs(ifft(Iz)), X, Y);
+	else
+		if (i == 0)
+			TaPrav = FL;
+		endif
+		Iz = FL ./ R;
+		pgmwrite(["g_lincoln_" int2str(i) ".pgm"], abs(ifft(Iz)), X, Y);
+	endif
 endfunction
 
-lincoln(0)
+for i=0:4
+	lincoln(i,0);
+	lincoln(i,1);
+endfor
--- Modelska Analiza/113/wiener.m
+++ Modelska Analiza/113/wiener.m
@@ -1,3 +1,8 @@
+global OrigSignal;
+global R;
+global CurrentC;
+global OrigS;
+
 c = [];
 for i = 0:3
 	c = [c, dlmread(["signal" int2str(i) ".dat"])];
@@ -19,19 +24,50 @@ function r = rob(x,y)
 	r = zeros(x,y);
 endfunction
 
+function [Sig, Phi] = dekonsignal(C, Phi, popravi)
+	global R
+	global CurrentC
+	D = abs(C).^2;
+	if (popravi)
+		CurrentC = C;
+		Phi = sqp(Phi, @napaka_suma, [], []);
+	endif
+	Sig = abs(ifft(C .* Phi ./ R));
+endfunction
+
+function n = napaka_suma(Phi)
+	global R;
+	global OrigSignal;
+	global CurrentC;
+	s = abs(ifft(CurrentC .* Phi ./ R));
+	n = sumsq(s - OrigSignal);
+endfunction
+
 N = [];
 N(:,1) = zeros(m, 1);
-N(:,2) = [0.01 * rob(50, 1); .99*D(51:462,1); 0.01*rob(50,1)];
-N(:,3) = [rob(30, 1); .99*D(31:482,2); rob(30,1)];
+N(:,2) = [0.01 * rob(50, 1); D(51:462,1); 0.01*rob(50,1)];
+N(:,3) = [rob(30, 1); D(31:482,2); rob(30,1)];
 N(:,4) = [100 * rob(20, 1); D(21:492,3); 100*rob(20,1)];
-
-Phi = ones(size(D)) - N ./ D
 
 r = exp(-abs(linspace(-255,256, m))/16)';
 R = fft(r);
 R(abs(R)<=eps) = 1;
 
-s = abs(ifft(C .* Phi ./ [R R R R]));
+
+[s, p] = dekonsignal(C(:,1), ones(m,1), 0);
+OrigSignal = s;
+
+for i = 2:4
+	[ss, pp] = dekonsignal(C(:,i), abs( C(:,1) ./ C(:,i) ).^2, 0);
+	s = [s, ss];
+	p = [p, pp];
+endfor
+
+Splosno = dekonsignal(C(:,1), ones(m,1), 0);
+CR = conj(R);
+for i = 2:4
+	Splosno = [Splosno, dekonsignal(C(:,i), ones(m,1) - N(:,i) ./ D(:,i), 0)];
+endfor
 
 clf
 plot(x, s(:,1), x, s(:,2))
@@ -39,4 +75,9 @@ legend("signal0.dat", "signal1.dat", "signal2.dat", "signal3.dat")
 xlabel("Cas $t$")
 ylabel("$s(t)$")
 print -depslatex g_signal "-S640,480" 
+
+save g_wiener_input.dat D
+save g_wiener_phi.dat p
+save g_wiener_rezultat.dat s
+save g_wiener_splosno.dat Splosno
 
