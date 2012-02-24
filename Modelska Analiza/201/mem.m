300025840b936597bb2b25ed4f0c98c943cc9154
Miha Čančula <miha@noughmad.eu>
2012-02-24 21:25:24 +0100

    Delal na nalogi 201

--- Modelska Analiza/201/mem.m
+++ Modelska Analiza/201/mem.m
@@ -1,29 +1,66 @@
 pkg load tsa
 
-function [P,R] = mem(Podatki, K)
+function S = mem(Podatki, K, F)
     N = max(size(Podatki));
     [AR,RC,PE] = durlev(acovf(Podatki, K));
     P = ar2poly(AR);
     R = roots(P);
-endfunction
 
-function naredi(Ime, K)
-    D = dlmread([Ime ".dat"])';
-    [P,R] = mem(D, K);
-    
-    n = max(size(D))
-    x = 1:n;
-    z = exp(-2*pi*i*x/n);
+    x = 1:N;
+    z = exp(-2*pi*i*x/N);
     S = 1 ./ abs(polyval(P,z)).^2;
     
-    F = abs(fft(D)).^2;
-    
-    S = S .* mean(F ./ S, "g");
-    
-    plot(x, S, x, F);
-    legend("Priblizek", "Podatki");
-    print("-depslatex", "S480,320", ["g_" Ime "_psd.tex"])
+    S = S .* mean(F ./ S, "h");
+	S = S(1:N/2);
 endfunction
 
-naredi("val2", 20);
-naredi("val3", 20);
+function naredi(Ime)
+    D = dlmread([Ime ".dat"])';
+
+	n = max(size(D));
+	m = min(size(D));
+
+	if (m > 1)
+		D = D(m,:);
+		co = polyfit(1:n, D, 1);
+		D = D - polyval(co, 1:n);
+	endif
+    F = abs(fft(D)).^2;
+	
+	x = 1:n/2;
+	
+    semilogy(x, F(x), x, mem(D,30,F), x, mem(D,20,F), x, mem(D,10,F));
+    legend("Podatki", "$m=30$", "$m=20$", "$m=10$");
+    print("-depslatex", "-S640,480", ["g_" Ime "_psd.tex"])
+endfunction
+
+function plotroots(Ime)
+	D = dlmread([Ime ".dat"])';
+	K = 20;
+
+	n = max(size(D));
+	m = min(size(D));
+
+	if (m > 1)
+		D = D(m,:);
+		co = polyfit(1:n, D, 1);
+		D = D - polyval(co, 1:n);
+	endif
+
+    [AR,RC,PE] = durlev(acovf(D, K));
+    P = ar2poly(AR);
+    R = roots(P);
+
+    x = 1:n;
+    z = exp(-2*pi*i*x/n);
+
+	plot(real(R), imag(R), "x", real(z), imag(z), "-");
+	legend("Poli", "Enotska kro\"znica")
+    print("-depslatex", "-S640,480", ["g_" Ime "_roots.tex"])
+endfunction
+
+naredi("val2");
+naredi("val3");
+naredi("co2");
+
+plotroots("co2");
