c6886e5ce2c2936e8fad18a4dc45cc8242b23726
Miha Čančula <miha@noughmad.eu>
2012-04-03 19:27:49 +0200

    Racunanje v Octave

--- Modelska Analiza/206/opna.m
+++ Modelska Analiza/206/opna.m
@@ -8,6 +8,51 @@ function [i,j] = indeks(N,n)
     j = mod(n,N);
 endfunction
 
+function r = radij(N,i)
+	r = (i-0.5)/N;
+
+endfunction
+
+function A = matrik_valj(nr,nfi)
+	c = (pi*nr/nfi).^-2;
+	D = ones(2/c);
+	n = nr*nfi;
+	A = zeros(n);
+	for i=1:nr
+		for j=1:nfi
+			s = st(nfi,i,j);
+			## Diagonalni clen
+			A(s,s) = 2*c/radij(nr,i)^2 + 2;
+
+			# Robni pogoji, vsi so prve vrste
+			if i == 1
+				A(s,s) = A(s,s) + 1;
+			else
+				A(s,st(nfi,i-1,j)) = -(1-0.5/i);
+			endif
+
+			if i == nr
+				A(s,s) = A(s,s) + 1;
+			else
+				A(s,st(nfi,i+1,j)) = -(1+0.5/i);
+			endif
+
+			if j == 1
+				A(s,s) = A(s,s) + c/radij(nr,i)^2;
+			else
+				A(s,st(nfi,i,j-1)) = -c/radij(nr,i)^2;
+			endif
+
+			if j == nfi
+				A(s,s) = A(s,s) + c/radij(nr,i)^2;
+			else
+				A(s,st(nfi,i,j+1)) = -c/radij(nr,i)^2;
+			endif
+		endfor
+	endfor
+	A = sparse(A);
+endfunction
+
 function A = matrika(n)
     # Stirice na diagonali
     A = 4*speye(n*n);
@@ -37,16 +82,29 @@ function B = masa(n, c)
     B = sparse(diag(reshape(B,n*n,1)));
 endfunction
 
-function izracun(n,c,st)
+function izracun(n,c,stv)
     A = matrika(n);
     B = masa(n,c);
-    [Vektorji, Vrednosti] = eig(A,B);
+	SizeA = size(A)
+	SizeB = size(B)
+    [Vektorji, Vrednosti] = eigs(A,B,stv,'sm');
     for i = 1:st
         V = reshape(Vektorji(:,i),n,n);
-        save(["g_opna_" int2str(n) "_" num2str(c) "_" int2str(i) ".dat"], "V");
+        save(["g_opna_" int2str(n) "_" num2str(c) "_" int2str(stv-i+1) ".dat"], "V");
         Vrednosti(i,i)
     endfor
+	M = reshape(full(diag(B)),n,n);
+	save(["g_masa_" int2str(n) "_" num2str(c) ".dat"], "M");
 endfunction
 
-izracun(32, 2, 10);
-izracun(32, 20, 10);
+function izracun_valj(n,st)
+	nr = n;
+	nfi = 2*n;
+    A = matrik_valj(nr,nfi);
+    [Vektorji, Vrednosti] = eigs(A,st,'sm');
+	diag(Vrednosti)
+    for i = 1:st
+        V = reshape(Vektorji(:,i),nfi,nr);
+        save(["g_valj_" int2str(n) "_" int2str(st-i+1) ".dat"], "V");
+    endfor
+endfunction
