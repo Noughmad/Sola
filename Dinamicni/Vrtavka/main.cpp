a460c57b4c105cb001735626b08f58c973173314
Miha Čančula <miha@noughmad.eu>
2012-06-22 19:26:31 +0200

    Izracunaj energijo in delez kaoticnega faznega prostora

--- Dinamicni/Vrtavka/main.cpp
+++ Dinamicni/Vrtavka/main.cpp
@@ -19,6 +19,13 @@ struct top_params
     double mg;
 };
 
+inline double energy(const top_params& top, double y[])
+{
+    return 0.5 * (y[0]*y[0] + y[1]*y[1] + top.D*y[2]*y[2]) + top.mg * (top.L * n(1) + top.a * n(2));
+}
+
+void random_state(double y[], const top_params& top, double emax);
+
 double lyapunov(const top_params& top, const double initial[]);
 
 inline double interpolate(double t1, double t2, double x1, double x2, double t)
@@ -29,17 +36,17 @@ inline double interpolate(double t1, double t2, double x1, double x2, double t)
 int odvod(double t, const double y[], double dy[], void* params)
 {
     top_params& p = *(top_params*)params;
-    
+
     const double R = 1.0-1.0/p.D;
-    
+
     dy[0] = y[1] * y[2] * R + p.mg * p.a * y[4];
     dy[1] = -y[2] * y[0] * R + p.mg * (p.L * y[5] - p.a * y[3]);
     dy[2] = -p.mg * p.L * y[4];
-    
+
     dy[3] = y[1] * y[5] - y[2] * y[4] / p.D;
     dy[4] = y[2] * y[3] / p.D - y[0] * y[5];
     dy[5] = y[0] * y[4] - y[1] * y[3];
-    
+
     return GSL_SUCCESS;
 }
 
@@ -48,12 +55,12 @@ class TopWorkspace
 public:
     TopWorkspace(double D, double a, double L);
     ~TopWorkspace();
-    
+
     void setInitial(double l1, double l2, double l3, double n1, double n2, double n3);
     void setInitial(const double initial[]);
-    
+
     double operator-(const TopWorkspace& other);
-    
+
     void poincare();
     inline void apply()
     {
@@ -63,11 +70,16 @@ public:
         }
         gsl_odeiv2_driver_apply(driver, &t, t+t1, y);
     }
-    
+
+    inline double energy()
+    {
+	return ::energy(params, y);
+    }
+
 public:
     double y[6];
     double t;
-    
+
 private:
     gsl_odeiv2_system system;
     gsl_odeiv2_driver* driver;
@@ -81,10 +93,10 @@ TopWorkspace::TopWorkspace(double D, double a, double L)
 {
     params = {D, a, L, 1.0};
     system = {odvod, 0, 6, &params};
-    driver = gsl_odeiv2_driver_alloc_y_new(&system, gsl_odeiv2_step_rk4, 1e-6, 1e-8, 1e-8);
-    
-    t1 = 1e-4;
-    
+    driver = gsl_odeiv2_driver_alloc_y_new(&system, gsl_odeiv2_step_rk4, 1e-4, 1e-4, 1e-4);
+
+    t1 = 1e-3;
+
     output.open("g_vrtavka.dat");
 }
 
@@ -99,7 +111,7 @@ void TopWorkspace::setInitial(double l1, double l2, double l3, double n1, double
   l(1) = l1;
   l(2) = l2;
   l(3) = l3;
-  
+
   n(1) = n1;
   n(2) = n2;
   n(3) = n3;
@@ -127,88 +139,145 @@ double TopWorkspace::operator-(const TopWorkspace& other)
 void TopWorkspace::poincare()
 {
     last[3] = y[3];
+    apply();
+    apply();
+    double tt = t;
 
     while (last[3] * y[3] > 0 || y[3] > last[3])
     {
         apply();
     }
-    
+
     double tS = t - t1 * fabs(y[3]) / (fabs(y[3]) + fabs(last[3]));
     
+ //   cout << "Poincare: perioda je " << tS - tt << endl; 
+
+    /*
     output << t;
     for (int i = 0; i < 6; ++i)
     {
         output << " " << interpolate(t-t1, t, last[i], y[i], tS);
     }
     output << endl;
+    */
+    for (int i = 0; i < 6; ++i)
+    {
+        y[i] = interpolate(t-t1, t, last[i], y[i], tS);
+    }
+    t = tS;
 }
 
 double lyapunov(const top_params& top, const double initial[])
 {
   double factor = 1;
-  const double step = 1e-6;
-  const double limit = 1e-3;
-  
-  int N = 1000;
-  
+  const double step = 1e-12;
+  const double limit = 1e-9;
+
+  const int N = 100;
+
   double z[6];
   for (int i = 0; i < 6; ++i)
   {
     z[i] = initial[i];
   }
-  
+
   TopWorkspace top1(top.D, top.a, top.L);
   top1.setInitial(z);
-  
+
   for (int i = 0; i < 6; ++i)
   {
     z[i] += (double)rand() / RAND_MAX * step;
   }
-  
-  ofstream out;
-  out.open("g_lyapunov.dat");
-  
+
   TopWorkspace top2(top.D, top.a, top.L);
   top2.setInitial(z);
   double d0 = top1 - top2;
+
+  for (int i = 0; i < N; ++i)
+  {
+      double d = top1 - top2;
+      
+      if (d > limit)
+      {
+          for (int i = 0; i < 6; ++i)
+          {
+              top2.y[i] = (1.0-step) * top1.y[i] + step * top2.y[i];
+          }
+          factor /= step;
+      }
+      top1.poincare();
+      top2.poincare();
+  }
+  
+  double d_mid = (top1 - top2) * factor;
+  factor = 1;
   
   for (int i = 0; i < N; ++i)
   {
-    double d = top1 - top2;
-    out << top1.t << " " << top2.t << " " << d << endl;
-    
-    if (d > limit)
-    {
-      cout << "Manjsam razliko";
-      for (int i = 0; i < 6; ++i)
+      double d = top1 - top2;
+      
+      if (d > limit)
       {
-        top2.y[i] = (1.0-step) * top1.y[i] + step * top2.y[i];
+          cout << "Manjsam razliko" << endl;
+          for (int i = 0; i < 6; ++i)
+          {
+              top2.y[i] = (1.0-step) * top1.y[i] + step * top2.y[i];
+          }
+          factor /= step;
       }
-      factor /= step;
-    }
-    top1.poincare();
-    top2.poincare();
+      top1.poincare();
+      top2.poincare();
   }
-  
-  out.close();
-  
-  double d = top1 - top2;
-  return log(d / d0) / top1.t;
+
+  double d = (top1 - top2) * factor;
+  double l1 = log(d_mid / d0) / N;
+  double l2 = log(d / d_mid) / N;
+  return l2;
 }
 
+void random_state(double y[], const top_params& top, double emax)
+{
+    do 
+    {
+	for (int i = 1; i < 4; ++i)
+	{
+	    l(i) = 10 * (2 * (double)rand() / RAND_MAX - 1);
+	    n(i) = (double)rand() / RAND_MAX;
+	}
+    }
+    while (energy(top, y) > emax);
+}
+
+double chaos_part(const top_params& top, double emax)
+{
+    int N = 100;
+    double y[6];
+    double lambda_limit = 1e-3;
+    int C = 0;
+    
+    for (int i = 0; i < N; ++i)
+    {
+	random_state(y, top, emax);
+	double l = lyapunov(top, y);
+	cout << "Eksponent je " << l << endl;
+	if (l > lambda_limit)
+	{
+	    C++;
+	}
+    }
+    
+    return (double)C/N;
+}
 
 int main(int argc, char **argv) {
+    int seed = time(0);
+    srand(seed);
+    
     top_params params = {2.0, 1.0, 0.5, 1.0};
-    double y[] = {0.3, 0.1, 1.0, 0.9, 0.3, 0.0};
-    cout << lyapunov(params, y) << endl;
-    y[5] = 0.1;
-    cout << lyapunov(params, y) << endl;
-    y[5] = 0.2;
-    cout << lyapunov(params, y) << endl;
-    y[5] = 0.3;
-    cout << lyapunov(params, y) << endl;
-    y[5] = 0.4;
-    cout << lyapunov(params, y) << endl;
+    cout << "Delez kaosa je " << chaos_part(params, 10) << endl;
+    top_params params2 = {2.0, 1.0, 0.0, 1.0};
+    cout << "Delez kaosa je " << chaos_part(params2, 10) << endl;
+    
     
     return 0;
 }
