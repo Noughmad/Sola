887b7d8d6c86c59a572fc702c805f21153da71c3
Miha Čančula <miha@noughmad.eu>
2013-02-23 15:22:03 +0100

    Uporabil prave (fizikalne) hermitove polinome

--- Visje Racunske Metode/Naloga_1/racun.m
+++ Visje Racunske Metode/Naloga_1/racun.m
@@ -3,7 +3,7 @@ global N
 global tau
 global h
 
-L = 30;
+L = 50;
 N = 1000;
 h = 2*L/N;
 tau = h^2
@@ -21,16 +21,16 @@ endfunction
 function psi = koherentno_stanje(n, z)
   global N;
   global L;
-  x = linspace(-L+z, L+z, N)';
+  x = space() - z;
   H = zeros(N, 1);
   if n == 0
     H = ones(size(x));
   elseif n == 1
-    H = x;
+    H = 2*x;
   elseif n == 2
-    H = x.^2 - 1;
+    H = 4*x.^2 - 2;
   elseif n == 3
-    H = x.^3 - 3*x;
+    H = 8*x.^3 - 12*x;
   endif
   psi = H .* exp(-0.5 * x.^2) / pi^(1/4) / sqrt(2^n * factorial(n));
 endfunction
@@ -67,29 +67,43 @@ function U = eksplicitna(H)
   endfor
 endfunction
 
-function rac_eksp(lambda, n)
+function n = norma(psi)
+  global h;
+  n = psi' * psi * h;
+endfunction
+
+function E = energija(H, psi)
+  global h;
+  E = (psi' * H * psi * h) / (norma(psi));
+endfunction
+
+function rac_eksp(lambda, n, z)
   H = hamiltonian(lambda);
-  psi = stanje(n);
+  psi = koherentno_stanje(n, z);
   U = eksplicitna(H);
   x = space();
   
   
   for step = 1:1000
     psi = U * psi;
-    plot(x, psi);
+    plot(x, real(psi), x, imag(psi));
+    n = norma(psi)
+    E = energija(H, psi)
     usleep(30);
   endfor
 endfunction
 
-function rac_imp(lambda, n)
+function rac_imp(lambda, n, z)
   H = hamiltonian(lambda);
-  psi = koherentno_stanje(n,3);
+  psi = koherentno_stanje(n,z);
   [U,V] = implicitna(H);
   x = space();
   
   for step = 1:1000
     psi = U \ (V * psi);
-    plot(x, psi);
+    plot(x, real(psi), x, imag(psi));
+    n = norma(psi);
+    E = energija(H, psi);
     usleep(30);
   endfor
 endfunction
